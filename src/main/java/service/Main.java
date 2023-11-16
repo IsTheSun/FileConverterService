ea73e42359e5b966b90607571c5556bcd952cad5
IsTheSun <svet_lanaxxi@mail.ru>
2023-11-17 01:22:35 +0300

    Изменён класс Main

--- src/main/java/service/Main.java
+++ src/main/java/service/Main.java
@@ -2,9 +2,28 @@ package service;
 
 import service.converters.JsonToXml;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        JsonToXml converter = new JsonToXml();
-        converter.convert("doramas.json", "outdoramas.xml");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Выберите расширение файла, который будете преобразовывать:\n" +
+                "1).xml\n" +
+                "2).json");
+        int ex = in.nextInt();
+        switch (ex)
+        {
+            case 1:
+            {
+                break;
+            }
+            case 2:
+            {
+                JsonToXml converter = new JsonToXml();
+                converter.convert("doramas.json", "outdoramas.xml");
+                break;
+            }
+            default:
+        }
     }
 }
