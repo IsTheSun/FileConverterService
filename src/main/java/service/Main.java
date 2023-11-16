ed8c791420b4afb907c62b79915ed906c5d9dab4
IsTheSun <svet_lanaxxi@mail.ru>
2023-11-17 00:04:15 +0300

    Написан мэйн для проверки

--- src/main/java/service/Main.java
+++ src/main/java/service/Main.java
@@ -1,4 +1,10 @@
 package service;
 
+import service.converters.JsonToXml;
+
 public class Main {
+    public static void main(String[] args){
+        JsonToXml converter = new JsonToXml();
+        converter.convert("doramas.json", "outdoramas.xml");
+    }
 }
